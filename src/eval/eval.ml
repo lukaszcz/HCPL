59088f5b6347f30b23b39155fecc34ed9f632a9a
Łukasz Czajka <lukaszcz@mimuw.edu.pl>
2013-07-17 19:51:30 +0200

    Removed unused apply_lambda function

--- src/eval/eval.ml
+++ src/eval/eval.ml
@@ -33,35 +33,6 @@ let do_close x env env_len =
   | Var(n) -> Env.nth env n
   | _ -> Closure(x, env, env_len)
 
-let rec apply_lambda body frame call_type y a_env a_env_len env env_len =
-  assert (env_len >= frame);
-  let env2 = pop_to env env_len frame
-  and env2_len = frame
-  in
-  if call_type = CallByValue then
-    begin
-      assert (env_len >= frame);
-      let arg = do_eval y a_env a_env_len
-      in
-      do_eval body (arg :: env2) (env2_len + 1)
-    end
-  else
-    begin
-      match y with
-      | Force(arg) ->
-          do_eval body ((do_eval arg a_env a_env_len) :: env2) (env2_len + 1)
-      | Leave(arg) ->
-          do_eval body ((do_close arg a_env a_env_len) :: env2) (env2_len + 1)
-      | _ ->
-          let arg =
-            if call_type = CallByNeed then
-              Delayed(ref (do_close y a_env a_env_len))
-            else
-              do_close y a_env a_env_len
-          in
-          do_eval body (arg :: env2) (env2_len + 1)
-    end
-
 and do_eval node env env_len =
   match node with
   | Appl(x, y, attrs) ->
